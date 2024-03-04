ce165454711f280bba2e50aa8d9c3778c74fb04f
judey <cg@macbook-air.mynetworksettings.com>
2024-03-04 15:13:41 -0500

    Finished my js file and alert window

---
+++ index.js
@@ -0,0 +1,24 @@
+/* First I'll create my variable message, which will contain the string of the message detailing the secret combination */
+/* Next, I'll  then create my first variable, num1, which will come out to 10 */
+/* Third, I will make my second variable, num2, which will come out to 40 */
+/* Fourth, I will make my third variable, num3, which will come out to 39 */
+/* Fifth, I will make an alert window that combines my variables message, num1, num2 and num3, with spaces and dashes added in */
+/* Note, I used console log just for the habit, but it should output the respective combination */
+
+const message = 'You have received this message because you have been chosen to open an important vault. Here is the secret combination:'
+
+const num1 = 5 + 5;
+console.log (num1);
+
+const num2 = num1 * 4;
+console.log (num2); 
+
+const num3 = 78 / 2;
+console.log (num3);
+
+alert(message + ' ' + num1 + '-' + num2 + '-' + num3);
+
+
+
+
+
